277941dae47f0c00943dbf04c2791bd004aa809d
AmrMashal <49400485+AmrMashal@users.noreply.github.com>
2019-10-10 11:24:02 +0100

    Update jenkins_script.sh

--- jenkins_script.sh
+++ jenkins_script.sh
@@ -1,23 +1,8 @@
-# #! /bin/bash
-
-# sudo apt-get update
-# sudo apt-get install -y apache2
-# sudo systemctl start apache2
-# sudo systemctl enable apache2
-# echo "<h1>Hello Amr, does this work?</h1>" | sudo tee /var/www/html/index.html
-
-# #! /bin/bash
-
-
-# sudo apt update -y
-# sudo apt install openjdk-8-jdk -y
-# wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-# sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-# sudo apt update -y
-# sudo apt install jenkins -y
-# sudo ufw allow 8080
-# sudo apt install awscli -y
-
 #! /bin/bash
 
-echo "<h1>Hello Amr, does this work?</h1>" | sudo tee /home/ubuntu/testamr.html
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+echo "<h1>Hello Amr, does this work?</h1>" | sudo tee /var/www/html/index.html
+
